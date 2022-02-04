fc9927774d3340cfde6e363a61a77e62059b1721
80HDev <98881478+80HDev@users.noreply.github.com>
2022-02-04 22:44:55 +0000

    Add files via upload

---
+++ learn-python.py
@@ -0,0 +1,18 @@
+# learning Python
+# anthony Eccles
+
+
+
+
+
+def main():
+   print("Keep it logically awesome.")
+
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+
+  #print(quotes)
+
+if __name__== "__main__":
+  main()
